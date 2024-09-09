3c74454565778b5b458e1a0ebc10c30d08580412
scott062 <scott.m.burdick@gmail.com>
2024-09-09 18:35:27 -0400

    Update commands and search without account specific api calls

--- auth.py
+++ auth.py
@@ -6,15 +6,29 @@ from dotenv import load_dotenv
 
 def authenticate():
     load_dotenv()
-    url = "https://accounts.spotify.com/api/token"
+
     CLIENT_ID = os.getenv("CLIENT_ID")
     CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+    url = "https://accounts.spotify.com/api/token"
     payload = {"grant_type": "client_credentials"}
     headers = {"Content-Type" : "application/x-www-form-urlencoded"}
-    req = requests.post(url, data=payload, auth=HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET), headers=headers)
-    print(req.json())
 
+    res = requests.post(url, data=payload, auth=HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET), headers=headers)
 
+    if res.ok:
+        token = res.json().get("access_token")
+        write_token(token)
+        return read_token()
+    return res.raise_for_status()
 
+def write_token(token):
+    with open("token.txt", "w") as f:
+        f.write(token)
+
+def read_token():
+    with open("token.txt", "r") as f:
+        content = f.read()
+        return content
 
 
--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from auth import authenticate
+import os
+from commands import commands_lookup, search
 
 def main():
-    authenticate()
-    print("hello world")
+    try:
+        authenticate()
+        while True:
+            cmd = input("Please type the number of the desired command:\n1. Search\n2. Pause\n3. Go Back\n4. Go Next\n")
+            print(search())
+            print(f"you typed: {cmd}")
+            if cmd in commands_lookup:
+                print("good command...")
+                commands_lookup[cmd]
+            else:
+                print("Invalid command, try again")
+    finally:
+        print("deleting token before ending")
+        os.remove("token.txt")
+        print("done")
 
 main()
+
 
